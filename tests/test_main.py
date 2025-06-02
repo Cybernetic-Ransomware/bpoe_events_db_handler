7a1a2b0d40724109763e44e0ccd8fc84d59b186f
Cybernetic-Ransomware <71835339+Cybernetic-Ransomware@users.noreply.github.com>
2025-06-02 16:04:14 +0200

    Hotfix: mocking config

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,10 @@
 import pytest
 from httpx import ASGITransport, AsyncClient
 
-from src.main import app
-
 
 @pytest.mark.asyncio
-async def test_docs_endpoint():
+async def test_docs_endpoint(app):
     transport = ASGITransport(app=app)
-    async with AsyncClient(transport=transport, base_url="http://127.0.0.1:8000") as client:
+    async with AsyncClient(transport=transport, base_url="http://testserver") as client:
         response = await client.get("/docs")
     assert response.status_code == 200

da450655f97b870dd28f96d8f33c131e03e5cf5a
Cybernetic-Ransomware <71835339+Cybernetic-Ransomware@users.noreply.github.com>
2026-06-12 21:45:02 +0200

    style(relationaldb): fix extra blank line in exceptions.py

--- src/core/relationaldb/exceptions.py
+++ src/core/relationaldb/exceptions.py
@@ -11,7 +11,6 @@ class ConnectionNotEstablishedError(HTTPException):
         super().__init__(status_code=500, detail=f"Connection not established. Call connect() first.\n{message}")
 
 
-
 class RecordUpdateNotAllowedError(HTTPException):
     def __init__(self, code: int = 404, message: str = ""):
         super().__init__(status_code=code, detail=f"Record can not be modified\n {message}")
